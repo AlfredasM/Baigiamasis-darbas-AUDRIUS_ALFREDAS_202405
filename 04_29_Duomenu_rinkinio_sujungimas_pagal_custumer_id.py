c6ad1ba9a7ac8879f54a2ea7ad529cf04e79a98d
AlfredasM <163419555+AlfredasM@users.noreply.github.com>
2024-04-29 14:34:43 +0300

    Update 04_29_Duomenu_rinkinio_sujungimas_pagal_custumer_id.py

--- 04_29_Duomenu_rinkinio_sujungimas_pagal_custumer_id.py
+++ 04_29_Duomenu_rinkinio_sujungimas_pagal_custumer_id.py
@@ -1,1 +1,24 @@
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
+df1 = pd.read_excel('99Bikers_Raw_data.xlsx', sheet_name='Transactions')
+df2 = pd.read_excel('99Bikers_Raw_data.xlsx', sheet_name='CustomerDemographic')
+
+# Sujungiame excleio du datasheet
+data = pd.merge(df1, df2, on='customer_id')
+# print(data)
+# sutvarkome duomenis
+data['gender'] = data['gender'].replace({'M': 'Male', 'Femal': 'Female', 'U': 'Unknown', 'F': 'Female'})
+#suskaiciuojame kiek yra skirtingu reiksmiu
+kiek_pirko_pagal_lyti = data['gender'].value_counts()
+print(kiek_pirko_pagal_lyti)
+
+#nusibraizome grafika
+sns.barplot(x=kiek_pirko_pagal_lyti.index, y=kiek_pirko_pagal_lyti.values, palette='Set2')
+plt.title('Koks yra pirkeju pasiskirstymas pagal lyti')
+plt.xlabel('Vyras/Moteris/Nezinomas')
+plt.ylabel('Kiekis')
+plt.legend()
+plt.show()
